49e97b46e196179f0522b37e0987f17625a145de
jesusmira <jesmira@hotmail.es>
2015-06-14 20:28:24 +0200

    Despliege DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,5 +1,7 @@
 var path = require('path');
 
+// Forma de cargar base de datos sqlite, (forma local)
+/*
 // Cargar modelo ORM
 var Sequelize = require('sequelize');
 
@@ -13,6 +15,53 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; // exportar definicion de la tabla Quiz
 
 // sequelize.sync() crea e inicializa la tabla de preguntas en DB
+sequelize.sync().success(function(){
+	Quiz.count().success(function(count){
+		//success(..) ejecuta el manejador una vez creada la tabla
+		if (count === 0) { // la tabla se inicializa solo si está vacía
+			Quiz.create({ pregunta: 'Capital de Italia',
+						  respuesta: 'Roma'
+						})
+			.success(function(){console.log('Base de datos inicializada')});		
+
+		};
+	});
+});
+*/
+
+// Forma de cargar base de datos Postgres, (Forma remota en heroku)
+// Postgres DATABASE_URL = postgress://user:passswd@host:port/database
+// SQLite   DATABASE_URL = sqlite://
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+// Cargar modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+	{ dialect : protocol,
+	  protocol:	protocol,
+	  port    : port,
+	  host    : host,
+	  storage : storage, //solo SQLite (.env)
+	  omitNull: true
+	}
+);
+
+// Importar definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+exports.Quiz = Quiz; //exportar la tabla Quiz
+
 sequelize.sync().success(function(){
 	Quiz.count().success(function(count){
 		//success(..) ejecuta el manejador una vez creada la tabla
