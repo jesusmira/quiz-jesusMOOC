dd89e7161b8f5b9e19e40a71dd3647f4a4b41f73
jesusmira <jesmira@hotmail.es>
2015-06-30 19:51:21 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,6 +11,11 @@ router.get('/', function(req, res) {
 });
 // Autoload de comados con :quizId
 router.param('quizId', quizController.load);
+
+//Definicion de rutas de sesion
+router.get('/login', sessionController.new);      // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Definicion de rutas quizes( con solo un elemento)
 /*
@@ -19,14 +26,18 @@ router.get('/quizes/answer', quizController.answer);
 router.get('/quizes/',                     quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-// Añadimos la definicion de rutas para crear elementos en BD
+// Añadimos la definicion de rutas para crear elementos en BD(quiz)
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
 // Añadimos la definicion de rutas para editar los elementos de la BD
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 // Añadimos la definicion de ruta para borrar elementos en la BD
-router.delete('/quizes/:quizId(\\d+)',        quizController.destroy);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
+// Añadimos la definicion de rutas para crear elementos en BD(comment)
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+
 
 // GET /author
 router.get('/author', function(req, res) {
